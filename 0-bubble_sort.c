e507391b7fc0e9e09d7d71f04bd021771210fed0
Usool-Data-Science <adeshinaibrahim10@gmail.com>
2024-01-22 22:04:42 +0100

    Solve task 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include "sort.h"
+
+/**
+  * bubble_sort - Sort the @array in ascending order
+  * @array: The array to sort
+  * @size: The length of the array
+  */
+void bubble_sort(int *array, size_t size)
+{
+	int temp;
+	bool swapper;
+	size_t j, i;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		swapper = false;
+		for (j = 0; j < size -i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				swapper = true;
+				print_array(array, size);
+			}
+		}
+		if (swapper == false)
+			break;
+	}
+}
